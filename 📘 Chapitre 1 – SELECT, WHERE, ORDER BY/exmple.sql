bb17b9e7aeec02ce2c6cb336e85e7262620f81e9
AyoubAmaghouch <ayoubamaghouche@gmail.com>
2026-01-05 09:46:58 +0000

    push

--- 📘 Chapitre 1 – SELECT, WHERE, ORDER BY/exmple.sql
+++ 📘 Chapitre 1 – SELECT, WHERE, ORDER BY/exmple.sql
@@ -43,3 +43,20 @@ CREATE TABLE commentaire (
     FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur),
     FOREIGN KEY (id_article) REFERENCES article(id_article) 
 );
+
+SELECT * FROM utilisateur;
+
+SELECT titre, contenu FROM utilisateur;
+
+SELECT titre, date_pub
+FROM Article
+WHERE date_pub >= '2024-01-01';
+
+SELECT titre, date_pub
+FROM Article
+ORDER BY date_pub DESC;
+
+SELECT titre
+FROM Article
+ORDER BY date_pub DESC
+LIMIT 5;
